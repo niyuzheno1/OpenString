f2aef67696d1fbd6f099eade9e326fa7294df924
Zack Ni <yn117@scarletmail.rutgers.edu>
2020-10-26 17:57:59 -0700

    Update ScientificConstants.py

--- ScientificConstants.py
+++ ScientificConstants.py
@@ -1,7 +1,7 @@
 #
 #
 #
-# Copyright (C) 2020 Zach (Yuzhe) Ni
+# Copyright (C) 2020 Zach (Yuzhe) Ni [First name] [Last Name]
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
