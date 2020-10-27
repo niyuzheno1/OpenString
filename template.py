a05e1cb90045d0522b214656eb3f77899ac0289e
Zack Ni <yn117@scarletmail.rutgers.edu>
2020-10-26 17:52:25 -0700

    Create template.py

---
+++ template.py
@@ -0,0 +1,45 @@
+#
+#
+#
+# Copyright (C) 2020 [First name] [Last Name]
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+#
+# [briefly explain what the python file does]
+#
+
+
+# [ a description of the inputs 
+#   name of the arguments and their type
+#   a description of the return value
+#   a description of the functionality of the procedure
+# e.g. 
+# The function templatefunc has two arguments arg1 and arg2.
+# The argument arg1 has Nonetype. It's for no reason.
+# The argument arg2 has Nonetype. It's for no reason.
+# The return value of the function templatefunc is of void type.
+# The functionality of our function templatefunc is for the purpose of demo]
+def templatefunc(arg1, arg2):
+  pass
+  
+# comments for branching
+# if : # in [what case], we do [what]
+# elif :  # in [what case], we do [what]
+# else :  # Otherwise, we do [what]
+
+# comments for define classes 
+# The class sampleclass is our sample class. 
+# It has 0 fields. 
+class sampleclass:
+    # we first initialize our fields into...
+    def __init__(self):
+      pass
+    # The string format for class sampleclass is ...
+    def __str__(self):
+      pass
+# comments for defining variables
+# comm_value = 0 # the common value for the python file
