0b91d5757ee1c5b16855a306bd80420dd5d6cac8
Zack Ni <yn117@scarletmail.rutgers.edu>
2020-10-26 17:57:40 -0700

    Create ScientificConstants.py

---
+++ ScientificConstants.py
@@ -0,0 +1,17 @@
+#
+#
+#
+# Copyright (C) 2020 Zach (Yuzhe) Ni
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+#
+# This python file has established some scientific constants such as the speed of light, Planck units and other useful metrics for our project
+#
+
+
+ SPEED_OF_LIGHT = 299792458 # The speed of light, dimension = meters per second,
+ GRAVUTATIONAL_CONSTANTS = 6.6743015e-11 # gravitational constants, dimension = meters to the cubes times kilogram to the minus one times seconds to minus two 
